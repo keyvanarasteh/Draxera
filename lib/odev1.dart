e92ba19e4d81195a81ea4a40f77c80b4a103a17a
umutturna <211216037@stu.istinye.edu.tr>
2023-01-02 13:57:49 +0300

    appbar drawer ve ımageler yüklendi

--- lib/odev1.dart
+++ lib/odev1.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unnecessary_const, unused_local_variable, non_constant_identifier_names, prefer_const_constructors, duplicate_ignore
+// ignore_for_file: unnecessary_const, unused_local_variable, non_constant_identifier_names, prefer_const_constructors, duplicate_ignore, dead_code, unused_label
 
 import 'dart:js';
 
@@ -31,71 +31,90 @@ class _HomescreenState extends State<Homescreen> {
 
     var DeviceTYpe = checkDevice(scrWidth);
 
-    return Scaffold(
-        appBar: AppBar(
-          title: Center(
-            child: Text('thy'),
-          ),
-          centerTitle: true,
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.search),
-              onPressed: () {},
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            title: Center(
+              child: CircleAvatar(
+                radius: 20,
+                backgroundImage: AssetImage("assets/thy.png"),
+              ),
             ),
-            IconButton(
-              icon: const Icon(Icons.login),
-              onPressed: () {},
-            ),
-          ],
-          backgroundColor: Colors.black,
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              const DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.greenAccent,
-                ),
-                child: Text('THY LOGO'),
+            centerTitle: true,
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: () {},
               ),
-              ListTile(
-                title: const Text('planla ve uç'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: const Text('seyahat deneyimi'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: const Text('hizmetlerimiz'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: const Text('kampanyalar'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: const Text('iletişim'),
-                onTap: () {},
+              IconButton(
+                icon: const Icon(Icons.login),
+                onPressed: () {},
               ),
             ],
+            backgroundColor: Colors.black,
           ),
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 40),
-            child: Center(
-              child: Column(
-                children: <Widget>[
-                  Image.network(
-                      'https://images.pexels.com/photos/1266831/pexels-photo-1266831.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
-                  IntrinsicHeight(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Expanded(
-                          child: Padding(
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                const DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.greenAccent,
+                  ),
+                  child: Image(image: AssetImage("assets/thy.png")),
+                ),
+                ListTile(
+                  title: const Text('planla ve uç'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: const Text('seyahat deneyimi'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: const Text('hizmetlerimiz'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: const Text('kampanyalar'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: const Text('iletişim'),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 40),
+              child: Center(
+                child: Column(
+                  children: <Widget>[
+                    Image.network(
+                        'https://images.pexels.com/photos/1266831/pexels-photo-1266831.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+                    IntrinsicHeight(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Container(
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: <Widget>[
+                                    Image.network(
+                                        'https://images.pexels.com/photos/417344/pexels-photo-417344.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                              child: Padding(
                             padding: const EdgeInsets.all(20.0),
                             child: Container(
                               child: Column(
@@ -103,32 +122,27 @@ class _HomescreenState extends State<Homescreen> {
                                     MainAxisAlignment.spaceEvenly,
                                 children: <Widget>[
                                   Image.network(
-                                      'https://images.pexels.com/photos/417344/pexels-photo-417344.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')
+                                      'https://images.pexels.com/photos/753626/pexels-photo-753626.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')
                                 ],
                               ),
                             ),
-                          ),
-                        ),
-                        Expanded(
-                            child: Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Container(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: <Widget>[
-                                Image.network(
-                                    'https://images.pexels.com/photos/753626/pexels-photo-753626.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')
-                              ],
-                            ),
-                          ),
-                        ))
-                      ],
+                          ))
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-        ));
+          )),
+    );
+    bottomNavigationBar:
+    BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+        ),
+      ],
+    );
   }
 }
