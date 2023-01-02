417fa46f3122ec2348e197b6369d2e8ebb86ee34
umutturna <211216037@stu.istinye.edu.tr>
2023-01-02 21:18:43 +0300

    tabbar eklendi

--- lib/odev1.dart
+++ lib/odev1.dart
@@ -32,125 +32,153 @@ class _HomescreenState extends State<Homescreen> {
     var DeviceTYpe = checkDevice(scrWidth);
 
     return SafeArea(
-      child: Scaffold(
-          bottomNavigationBar: BottomNavigationBar(
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.search),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.bus_alert),
-                label: '',
-              )
-            ],
-          ),
-          appBar: AppBar(
-            title: Center(
-              child: CircleAvatar(
-                radius: 20,
-                backgroundImage: AssetImage("assets/thy.png"),
-              ),
-            ),
-            centerTitle: true,
-            actions: <Widget>[
-              IconButton(
-                icon: const Icon(Icons.search),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: const Icon(Icons.login),
-                onPressed: () {},
-              ),
-            ],
-            backgroundColor: Colors.black,
-          ),
-          drawer: Drawer(
-            child: ListView(
-              padding: EdgeInsets.zero,
-              children: [
-                const DrawerHeader(
-                  decoration: BoxDecoration(
-                    color: Colors.greenAccent,
-                  ),
-                  child: Image(image: AssetImage("assets/thy.png")),
+      child: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+            bottomNavigationBar: BottomNavigationBar(
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: '',
                 ),
-                ListTile(
-                  title: const Text('planla ve uç'),
-                  onTap: () {},
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.search),
+                  label: '',
                 ),
-                ListTile(
-                  title: const Text('seyahat deneyimi'),
-                  onTap: () {},
-                ),
-                ListTile(
-                  title: const Text('hizmetlerimiz'),
-                  onTap: () {},
-                ),
-                ListTile(
-                  title: const Text('kampanyalar'),
-                  onTap: () {},
-                ),
-                ListTile(
-                  title: const Text('iletişim'),
-                  onTap: () {},
-                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.bus_alert),
+                  label: '',
+                )
               ],
             ),
-          ),
-          body: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 40),
-              child: Center(
-                child: Column(
-                  children: <Widget>[
-                    Image.network(
-                        'https://images.pexels.com/photos/1266831/pexels-photo-1266831.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
-                    IntrinsicHeight(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Expanded(
-                            child: Padding(
+            appBar: AppBar(
+              bottom: const TabBar(tabs: [
+                Tab(
+                  icon: Icon(Icons.directions_car),
+                ),
+                Tab(
+                  icon: Icon(Icons.directions_boat),
+                ),
+                Tab(
+                  icon: Icon(Icons.directions_walk),
+                )
+              ]),
+              title: Center(
+                child: CircleAvatar(
+                  radius: 20,
+                  backgroundImage: AssetImage("assets/thy.png"),
+                ),
+              ),
+              centerTitle: true,
+              actions: <Widget>[
+                IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Icon(Icons.login),
+                  onPressed: () {},
+                ),
+              ],
+              backgroundColor: Colors.black,
+            ),
+            drawer: Drawer(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  const DrawerHeader(
+                    decoration: BoxDecoration(
+                      color: Colors.greenAccent,
+                    ),
+                    child: Image(image: AssetImage("assets/thy.png")),
+                  ),
+                  ListTile(
+                    title: const Text('planla ve uç'),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    title: const Text('seyahat deneyimi'),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    title: const Text('hizmetlerimiz'),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    title: const Text('kampanyalar'),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    title: const Text('iletişim'),
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            ),
+            body: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 40),
+                child: Center(
+                  child: Column(
+                    children: <Widget>[
+                      Opacity(
+                        opacity: 0.8,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(18)),
+                          child: Image.network(
+                              'https://images.pexels.com/photos/1266831/pexels-photo-1266831.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+                        ),
+                      ),
+                      IntrinsicHeight(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: Container(
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: <Widget>[
+                                      ClipRRect(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(18)),
+                                        child: Image.network(
+                                            'https://images.pexels.com/photos/417344/pexels-photo-417344.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                                child: Padding(
                               padding: const EdgeInsets.all(20.0),
                               child: Container(
                                 child: Column(
                                   mainAxisAlignment:
                                       MainAxisAlignment.spaceEvenly,
                                   children: <Widget>[
-                                    Image.network(
-                                        'https://images.pexels.com/photos/417344/pexels-photo-417344.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')
+                                    ClipRRect(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(18)),
+                                      child: Image.network(
+                                          'https://images.pexels.com/photos/753626/pexels-photo-753626.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+                                    )
                                   ],
                                 ),
                               ),
-                            ),
-                          ),
-                          Expanded(
-                              child: Padding(
-                            padding: const EdgeInsets.all(20.0),
-                            child: Container(
-                              child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: <Widget>[
-                                  Image.network(
-                                      'https://images.pexels.com/photos/753626/pexels-photo-753626.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')
-                                ],
-                              ),
-                            ),
-                          ))
-                        ],
+                            ))
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          )),
+            )),
+      ),
     );
   }
 }
